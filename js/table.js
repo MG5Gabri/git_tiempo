7e07f8d30341c1b006a3c059518536066d5fdb7c
lglopez <lglopez@scl.edu.gt>
2024-05-22 11:35:00 -0600

    version 1ra rama

---
+++ js/table.js
@@ -0,0 +1,30 @@
+let li_tabla=document.querySelector("table")
+    li_tabla.innerHTML=`
+    <tr>
+        <td>1</td>
+        <td>git clone</td>
+        <td>Clonar o crear una copia del repositorio</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>git status</td>
+        <td>Muestra el estado del repositorio</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>git log</td>
+        <td>Muestra el historial de versiones del repositorio</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>git pull origin main</td>
+        <td>Baja todos los cambios o actualizaciones del repositorio</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>git status</td>
+        <td>Sube todos los cambios al repositorio</td>
+    </tr>
+
+
+    `;
