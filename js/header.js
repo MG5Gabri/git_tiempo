e4cc6b6e6806b6996eaa2faf2677ca2d45e2461a
lglopez <lglopez@scl.edu.gt>
2024-05-29 19:04:59 -0600

    version ramagl header

---
+++ js/header.js
@@ -0,0 +1,8 @@
+let li_header=document.querySelector(".header")
+    li_header.innerHTML=`
+        <div class="titulo">
+            <h1>Comandos</h1>
+            <h1 class="diseño">Git</h1>
+        </div>
+        <a href="" class="btn_git">GIT HUB</a>
+    `;
